8a82a703150f0912db303ba9d72cc638037e181d
Severino <snsf2134@gmail.com>
2026-02-18 20:33:38 -0300

    first commit

---
+++ src/main/java/com/seve/GerenciamentoPedidosSpring/GerenciamentoPedidosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.seve.GerenciamentoPedidosSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoPedidosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoPedidosSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seve/GerenciamentoPedidosSpring/GerenciamentoPedidosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seve.GerenciamentoPedidosSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoPedidosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
